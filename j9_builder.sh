84d970fcd3509a738f3b07ac2d13a8cf8565a8b1
jeanlego (Jean-Philippe Legault) <jeanphilippe.legault@unb.ca>
2020-03-03 22:29:20 -0400

    quote var

--- j9_builder.sh
+++ j9_builder.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ARGS_IN="$@"
+ARGS_IN=( "$@" )
 
 ## Returns errlvl 0 if $1 is a reachable git remote url 
 reachable_host() {
@@ -15,52 +15,52 @@ reachable_host() {
 J9_JDK_BASE="jdk${VERSION}"
 
 
-BASE_DIR=${PWD}/j9Builds
-DOWNLOADS=${BASE_DIR}/downloads
-LOGS=${BASE_DIR}/logs
-UTILS=${BASE_DIR}/utils
-BUILDER=${UTILS}/builder
-SCRIPTS=${UTILS}/scripts
+BASE_DIR="${PWD}"/j9Builds
+DOWNLOADS="${BASE_DIR}"/downloads
+LOGS="${BASE_DIR}"/logs
+UTILS="${BASE_DIR}"/utils
+BUILDER="${UTILS}"/builder
+SCRIPTS="${UTILS}"/scripts
 
 J9_JDK_DIR="${BASE_DIR}/${J9_JDK_BASE}"
-SOURCE_FLAGS=${BASE_DIR}/j9.env
+SOURCE_FLAGS="${BASE_DIR}"/j9.env
 TMP_DIR=$(mktemp -d)
 
-mkdir -p ${BASE_DIR}
-mkdir -p ${DOWNLOADS}
-mkdir -p ${UTILS}
-rm -Rf ${SCRIPTS} && mkdir -p ${SCRIPTS}
-rm -Rf ${BUILDER} && mkdir -p ${BUILDER}     
-rm -Rf ${LOGS} && mkdir -p ${LOGS}     
+mkdir -p "${BASE_DIR}"
+mkdir -p "${DOWNLOADS}"
+mkdir -p "${UTILS}"
+rm -Rf "${SCRIPTS}" && mkdir -p "${SCRIPTS}"
+rm -Rf "${BUILDER}" && mkdir -p "${BUILDER}"     
+rm -Rf "${LOGS}" && mkdir -p "${LOGS}"     
 
-mkdir -p ${J9_JDK_DIR}
-mkdir -p ${J9_JDK_DIR}/omr
-mkdir -p ${J9_JDK_DIR}/openj9
+mkdir -p "${J9_JDK_DIR}"
+mkdir -p "${J9_JDK_DIR}/omr"
+mkdir -p "${J9_JDK_DIR}/openj9"
 
 
 git_origin="https://github.com/CAS-Atlantic"
 get_source_origin="https://github.com/ibmruntimes"
 
-# if $(reacheable_host gitlab.casa.cs.unb.ca); then
-#         git_origin="http://gitlab.casa.cs.unb.ca/omr"
-#         case ${VERSION} in
-#                 11)     get_source_origin="http://gitlab.casa.cs.unb.ca/omr";;
-#         esac
-# fi
+if reacheable_host "gitlab.casa.cs.unb.ca"; then
+        git_origin="http://gitlab.casa.cs.unb.ca/omr"
+        case "${VERSION}" in
+                11)     get_source_origin="http://gitlab.casa.cs.unb.ca/omr";;
+        esac
+fi
 
 # check valid env
-case ${BUILD_TYPE} in                
+case "${BUILD_TYPE}" in                
 release|debug) ;;*)     die -1 "BUILD_TYPE=\"${BUILD_TYPE}\"  env variable can only be \"release\" or \"debug\"";;
 esac
 
-case ${VERSION} in
+case "${VERSION}" in
 8|9|10|11|12);;*)       die -1 "VERSION=\"${VERSION}\" env variable can only be \"8, 9, 10, 11 or 12\"";;
 esac
 
 print_script_env() {
-        printf "\
-BUILD_TYPE=${BUILD_TYPE}\n\
-VERSION=${VERSION}\n\
+        echo  "\
+BUILD_TYPE=${BUILD_TYPE}
+VERSION=${VERSION}
 ARCH=${ARCH}
 "
 }
@@ -69,25 +69,25 @@ ARCH=${ARCH}
 # USAGE PRINTER
 usage() {
 
-        printf \
-"\n\
-$0 \n\
-Usage:\n\
-    build     [<makefile targets>...]          build using makefile\n\
-    configure [<extra-configure-args> ... ]    trigger a configure \n\
+        echo \
+"
+$0 
+Usage:
+    build     [<makefile targets>...]          build using makefile
+    configure [<extra-configure-args> ... ]    trigger a configure
 
 Variables:
     VERSION             8 | 9 | 10 | 11 | 12
     BUILD_TYPE          debug | release
     ARCH                x86_64 | x86 | aarch64 ... <see openj9 hotspot release>
 "
-exit $1
+exit "$1"
 }
 
 die() {
-        printf "\
- == ERROR: $2\n\
-you called this program with \"${ARGS_IN}\" \n\
+        echo "\
+ == ERROR: $2
+you called this program with \"${ARGS_IN[*]}\" 
 "
 print_script_env
 
@@ -97,23 +97,23 @@ print_script_env
 exec_or_die() {
         
         echo "Executing ${FUNCNAME[1]}"
-        echo "$@" | tr -s " " > ${SCRIPTS}/${FUNCNAME[1]}.sh
-        chmod +x ${SCRIPTS}/${FUNCNAME[1]}.sh
-        ${SCRIPTS}/${FUNCNAME[1]}.sh 2>&1 |& tee ${LOGS}/${FUNCNAME[1]}.progress
+        echo "$@" | tr -s " " > "${SCRIPTS}/${FUNCNAME[1]}.sh"
+        chmod +x "${SCRIPTS}/${FUNCNAME[1]}.sh"
+        "${SCRIPTS}/${FUNCNAME[1]}.sh" 2>&1 |& tee "${LOGS}/${FUNCNAME[1]}.progress"
 
         if [ "0" != $? ]; then 
                 echo "Failed to do \"${SCRIPTS}/${FUNCNAME[1]}.sh\", see ${LOGS}/${FUNCNAME[1]}.failure"
-                mv ${LOGS}/${FUNCNAME[1]}.progress ${LOGS}/${FUNCNAME[1]}.failure
+                mv "${LOGS}/${FUNCNAME[1]}.progress" "${LOGS}/${FUNCNAME[1]}.failure"
                 exit $?
         else
-                mv ${LOGS}/${FUNCNAME[1]}.progress ${LOGS}/${FUNCNAME[1]}.success
+                mv "${LOGS}/${FUNCNAME[1]}.progress" "${LOGS}/${FUNCNAME[1]}.success"
         fi
 }
 
 fetch() {
 	OUTPUT="$1"
 	URL="$2"
-        [ ! -f ${OUTPUT} ] \
+        [ ! -f "${OUTPUT}" ] \
         && reachable_host "${URL}" \
         && exec_or_die "curl --output ${OUTPUT} ${URL}"
 
@@ -158,7 +158,7 @@ git_init() {
 }
 
 init_source_jdk() {
-        [ ! -f ${J9_JDK_DIR}/.git ] &&
+        [ ! -f "${J9_JDK_DIR}"/.git ] &&
                 exec_or_die "\
                         git -C ${J9_JDK_DIR} init &&
                         git -C ${J9_JDK_DIR} remote add origin ${get_source_origin}/openj9-openjdk-jdk${VERSION}.git
@@ -166,7 +166,7 @@ init_source_jdk() {
 }
 
 init_source_omr() {
-	[ ! -f ${J9_JDK_DIR}/omr/.git ] &&
+	[ ! -f "${J9_JDK_DIR}"/omr/.git ] &&
                 exec_or_die "\
                         git -C ${J9_JDK_DIR}/omr init &&
                         git -C ${J9_JDK_DIR}/omr remote add origin ${git_origin}/openj9-omr.git
@@ -174,7 +174,7 @@ init_source_omr() {
 }
 
 init_source_openj9() {
-        [ ! -f ${J9_JDK_DIR}/openj9/.git ] &&
+        [ ! -f "${J9_JDK_DIR}"/openj9/.git ] &&
                 exec_or_die "\
                         git -C ${J9_JDK_DIR}/openj9 init &&
                         git -C ${J9_JDK_DIR}/openj9 remote add origin ${git_origin}/openj9.git
@@ -239,8 +239,8 @@ source_env() {
 export BUILD_TYPE=${BUILD_TYPE}
 export VERSION=${VERSION}
 export ARCH=${ARCH}
-export FREEMARKER_PATH="${UTILS}/freemarker.jar"
-export JAVA_HOME="${UTILS}/bootjdk${VERSION}_${ARCH}"
+export FREEMARKER_PATH=${UTILS}/freemarker.jar
+export JAVA_HOME=${UTILS}/bootjdk${VERSION}_${ARCH}
 export PATH=${BOOTJDK_PATH}/bin:${PATH}
 export OMR_OPTIMIZE=0
 export OPTIMIZATION_FLAGS='-fno-inline -fstack-protector-all'
@@ -257,7 +257,7 @@ export CFLAGS='-O0 -g3'
 export j9_conf='--with-debug-level=slowdebug'
 export BUILD_CONFIG=slowdebug
 export CONF=slowdebug
-" > ${SOURCE_FLAGS}
+" > "${SOURCE_FLAGS}"
 
         if [ "${BUILD_TYPE}" != "debug" ]
         then 
@@ -265,13 +265,13 @@ export CONF=slowdebug
 unset j9_conf
 unset BUILD_CONFIG
 unset CONF
-" >> ${SOURCE_FLAGS}
+" >> "${SOURCE_FLAGS}"
         fi
 }
 
 patch_debug() {
-        sed -i "/--strip-debug/d" ${J9_JDK_DIR}/openj9/runtime/makelib/targets.mk.linux.inc.ftl
-        sed -i "/--strip-debug/d" ${J9_JDK_DIR}/omr/omrmakefiles/rules.linux.mk
+        sed -i "/--strip-debug/d" "${J9_JDK_DIR}/openj9/runtime/makelib/targets.mk.linux.inc.ftl"
+        sed -i "/--strip-debug/d" "${J9_JDK_DIR}/omr/omrmakefiles/rules.linux.mk"
 }
 
 clean_cmd() {
@@ -284,7 +284,7 @@ configure_cmd() {
         exec_or_die "\
                 source ${SOURCE_FLAGS}; \
                 chmod +x ./configure; \
-                bash configure --with-freemarker-jar=${FREEMARKER_PATH} --with-boot-jdk=${JAVA_HOME} ${j9_conf} $@ ;\
+                bash configure --with-freemarker-jar=${FREEMARKER_PATH} --with-boot-jdk=${JAVA_HOME} ${j9_conf} $$* ;\
                 popd; \
                 " 
 }
@@ -294,7 +294,7 @@ build_cmd() {
                 source ${SOURCE_FLAGS}; \
                 pushd ${J9_JDK_DIR}; \
                 chmod +x ${UTILS}/casa.watchdog.sh ;\
-                ${UTILS}/casa.watchdog.sh make $@ ;\
+                ${UTILS}/casa.watchdog.sh make $* ;\
                 popd ;\
                 "
 }
@@ -307,7 +307,7 @@ case "$(uname -s)" in
                 # nothing to do
         ;;*)
                 echo "This script can only be ran on a Linux host, exiting"
-                exit -1
+                exit 255
         ;;
 esac
 
@@ -326,7 +326,7 @@ pids=()
 
 [ "_${XDOCKER}" == "_" ] && source_env
 
-source ${SOURCE_FLAGS}
+source "${SOURCE_FLAGS}"
 
 ( get_freemarker && extract_freemarker )& pids+=( "$!" )
 
@@ -346,11 +346,11 @@ source ${SOURCE_FLAGS}
 
 # Wait for all processes to finish
 EXIT_CODE=0
-for p in $pids; do
-        if wait $p; then
+for p in "${pids[@]}"; do
+        if wait "${p}"; then
                 echo "Process $p success"
         else
-                EXIT_CODE=$(( 1 + ${EXIT_CODE} ))
+                EXIT_CODE=$(( 1 + EXIT_CODE ))
                 echo "Process $p fail"
         fi
 done
@@ -365,8 +365,8 @@ if [ "${EXIT_CODE}" == "0" ]; then
 
                 echo "Starting docker chroot environment"
                 echo "####"
-                chmod +x ${UTILS}/xdocker.sh
-                ${UTILS}/xdocker.sh -f ${BUILDER}/Dockerfile ${ARCH} ./
+                chmod +x "${UTILS}/xdocker.sh"
+                "${UTILS}/xdocker.sh" -f "${BUILDER}/Dockerfile ${ARCH}" ./
         else
                 case $1 in
                         configure)        
@@ -376,7 +376,7 @@ if [ "${EXIT_CODE}" == "0" ]; then
                                 build_cmd "${@:2}"
                         ;;
                         *)              
-                                die -1 "Invalid command $@"
+                                die -1 "Invalid command $*"
                         ;;
                 esac
         fi
