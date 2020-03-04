fe02b15313cc843bab36ec08251640b1cc7bfa56
jeanlego (Jean-Philippe Legault) <jeanphilippe.legault@unb.ca>
2020-03-03 23:03:33 -0400

    use xargs for multithread

--- j9_builder.sh
+++ j9_builder.sh
@@ -14,7 +14,6 @@ reachable_host() {
 [ "_${BUILD_TYPE}" != "_" ]     || BUILD_TYPE="release"
 J9_JDK_BASE="jdk${VERSION}"
 
-
 BASE_DIR="${PWD}"/j9Builds
 DOWNLOADS="${BASE_DIR}"/downloads
 LOGS="${BASE_DIR}"/logs
@@ -37,11 +36,10 @@ mkdir -p "${J9_JDK_DIR}"
 mkdir -p "${J9_JDK_DIR}/omr"
 mkdir -p "${J9_JDK_DIR}/openj9"
 
-
 git_origin="https://github.com/CAS-Atlantic"
 get_source_origin="https://github.com/ibmruntimes"
 
-if reacheable_host "gitlab.casa.cs.unb.ca"; then
+if reachable_host "gitlab.casa.cs.unb.ca"; then
         git_origin="http://gitlab.casa.cs.unb.ca/omr"
         case "${VERSION}" in
                 11)     get_source_origin="http://gitlab.casa.cs.unb.ca/omr";;
@@ -56,6 +54,24 @@ esac
 case "${VERSION}" in
 8|9|10|11|12);;*)       die -1 "VERSION=\"${VERSION}\" env variable can only be \"8, 9, 10, 11 or 12\"";;
 esac
+
+declare -A URL
+declare -A OUTPUT
+
+URL[freemarker]="https://sourceforge.net/projects/freemarker/files/freemarker/2.3.8/freemarker-2.3.8.tar.gz/download"
+OUTPUT[freemarker]="${DOWNLOADS}/freemarker.tgz"
+
+URL[bootjdk]="https://api.adoptopenjdk.net/v2/binary/nightly/openjdk${VERSION}?openjdk_impl=hotspot&os=linux&arch=${ARCH}&release=latest&type=jdk"
+OUTPUT[bootjdk]="${DOWNLOADS}/bootjdk${VERSION}_${ARCH}.tar.gz"
+
+URL[dockerfile]="https://raw.githubusercontent.com/CAS-Atlantic/openj9/52fa8dc53987972998512f45b91fe4cca268b652/buildenv/docker/jdk11/x86_64/ubuntu18/Dockerfile"
+OUTPUT[dockerfile]="${BUILDER}/Dockerfile"
+
+URL[watchdog]="https://raw.githubusercontent.com/CAS-Atlantic/openj9/aarch64_casa_watchdog_script/casa.watchdog.sh"
+OUTPUT[watchdog]="${UTILS}/casa.watchdog.sh"
+
+URL[xdocker]="https://raw.githubusercontent.com/CAS-Atlantic/xdocker/master/xdocker.sh"
+OUTPUT[xdocker]="${UTILS}/xdocker.sh"
 
 print_script_env() {
         echo  "\
@@ -91,146 +107,87 @@ you called this program with \"${ARGS_IN[*]}\"
 "
 print_script_env
 
-        usage $1
+        usage "$1"
 }
 
-exec_or_die() {
-        
-        echo "Executing ${FUNCNAME[1]}"
-        echo "$@" | tr -s " " > "${SCRIPTS}/${FUNCNAME[1]}.sh"
-        chmod +x "${SCRIPTS}/${FUNCNAME[1]}.sh"
-        "${SCRIPTS}/${FUNCNAME[1]}.sh" 2>&1 |& tee "${LOGS}/${FUNCNAME[1]}.progress"
+create_sh() {
+        script_name="${SCRIPTS}/${FUNCNAME[1]}.sh"
+        script_log_filename="${LOGS}/${FUNCNAME[1]}"
+        script_log="${script_log_filename}.progress"
+        script_fail="${script_log_filename}.failure"
+        script_pass="${script_log_filename}.success"
 
-        if [ "0" != $? ]; then 
-                echo "Failed to do \"${SCRIPTS}/${FUNCNAME[1]}.sh\", see ${LOGS}/${FUNCNAME[1]}.failure"
-                mv "${LOGS}/${FUNCNAME[1]}.progress" "${LOGS}/${FUNCNAME[1]}.failure"
-                exit $?
-        else
-                mv "${LOGS}/${FUNCNAME[1]}.progress" "${LOGS}/${FUNCNAME[1]}.success"
-        fi
+        echo "Making ${script_name}"
+        echo "
+        rm ${script_log_filename}*
+        (
+                $* 
+        ) &> ${script_log} && touch ${script_pass} && exit 0;
+        touch ${script_fail} && exit 1;" > "${script_name}"
+        chmod +x "${script_name}"
 }
 
-fetch() {
-	OUTPUT="$1"
-	URL="$2"
-        [ ! -f "${OUTPUT}" ] \
-        && reachable_host "${URL}" \
-        && exec_or_die "curl --output ${OUTPUT} ${URL}"
-
+run_sh() {
+        echo "${SCRIPTS}/*" | xargs -P4 -I{} /bin/bash -c '{}'
 }
 
-get_freemarker() {
-	fetch \
-		"${DOWNLOADS}/freemarker.tgz" \
-		"https://sourceforge.net/projects/freemarker/files/freemarker/2.3.8/freemarker-2.3.8.tar.gz/download"
-}
-
-get_bootjdk() {
-	fetch \
-		"${DOWNLOADS}/bootjdk${VERSION}_${ARCH}.tar.gz" \
-		"https://api.adoptopenjdk.net/v2/binary/nightly/openjdk${VERSION}?openjdk_impl=hotspot&os=linux&arch=${ARCH}&release=latest&type=jdk"
-}
-
-get_dockerfile() {
-	fetch \
-		"${BUILDER}/Dockerfile" \
-		"https://raw.githubusercontent.com/CAS-Atlantic/openj9/52fa8dc53987972998512f45b91fe4cca268b652/buildenv/docker/jdk11/x86_64/ubuntu18/Dockerfile"
-}
-
-get_watchdog() {
-	fetch \
-		"${UTILS}/casa.watchdog.sh" \
-		"https://raw.githubusercontent.com/CAS-Atlantic/openj9/aarch64_casa_watchdog_script/casa.watchdog.sh"
-}
-
-get_xdocker() {
-	fetch \
-		"${UTILS}/xdocker.sh" \
-		"https://raw.githubusercontent.com/CAS-Atlantic/xdocker/master/xdocker.sh"
-}
-
-git_init() {
-        [ ! -f $1/.git ] &&
-              exec_or_die "\
-                        git -C $1 init &&
-                        git -C ${J9_JDK_DIR} remote add origin ${get_source_origin}/openj9-openjdk-jdk${VERSION}.git"
-
-}
-
-init_source_jdk() {
-        [ ! -f "${J9_JDK_DIR}"/.git ] &&
-                exec_or_die "\
-                        git -C ${J9_JDK_DIR} init &&
+get_source_jdk() {
+        create_sh "
+                if [ ! -f ${J9_JDK_DIR}/.git ]; then
+                        git -C ${J9_JDK_DIR} init
                         git -C ${J9_JDK_DIR} remote add origin ${get_source_origin}/openj9-openjdk-jdk${VERSION}.git
-                        "
-}
-
-init_source_omr() {
-	[ ! -f "${J9_JDK_DIR}"/omr/.git ] &&
-                exec_or_die "\
-                        git -C ${J9_JDK_DIR}/omr init &&
-                        git -C ${J9_JDK_DIR}/omr remote add origin ${git_origin}/openj9-omr.git
-                        "
-}
-
-init_source_openj9() {
-        [ ! -f "${J9_JDK_DIR}"/openj9/.git ] &&
-                exec_or_die "\
-                        git -C ${J9_JDK_DIR}/openj9 init &&
-                        git -C ${J9_JDK_DIR}/openj9 remote add origin ${git_origin}/openj9.git
-                        "
-}
-
-fetch_branch_jdk() {
-        exec_or_die "\
-                git -C ${J9_JDK_DIR} fetch --progress origin openj9 &&
-                git -C ${J9_JDK_DIR} checkout --progress -b openj9 origin/openj9
+                fi
+                git -C ${J9_JDK_DIR}/omr fetch --progress origin openj9 &&
+                git -C ${J9_JDK_DIR}/omr checkout --progress -b master origin/openj9
                 "
 }
 
-fetch_branch_openj9() {
-        exec_or_die "
-                git -C ${J9_JDK_DIR}/openj9 fetch --progress origin master &&
+get_omr() {
+	
+        create_sh "
+                if [ ! -f ${J9_JDK_DIR}/omr/.git ]; then
+                        git -C ${J9_JDK_DIR}/omr init
+                        git -C ${J9_JDK_DIR}/omr remote add origin ${git_origin}/omr.git
+                fi
+                git -C ${J9_JDK_DIR}/omr fetch --progress origin master &&
+                git -C ${J9_JDK_DIR}/omr checkout --progress -b master origin/master
+                "
+}
+
+get_openj9() {
+        
+        create_sh "
+                if [ ! -f ${J9_JDK_DIR}/openj9/.git ]; then
+                        git -C ${J9_JDK_DIR}/openj9 init
+                        git -C ${J9_JDK_DIR}/openj9 remote add origin ${git_origin}/openj9.git
+                fi
+                git -C ${J9_JDK_DIR}/openj9 fetch --progress origin master
                 git -C ${J9_JDK_DIR}/openj9 checkout --progress -b master origin/master
                 "
 }
 
-fetch_branch_omr() {
-        exec_or_die "
-                git -C ${J9_JDK_DIR}/omr fetch --progress origin openj9 &&
-                git -C ${J9_JDK_DIR}/omr checkout --progress -b openj9 origin/openj9
+get_freemarker() {
+
+        create_sh  "
+                if [ ! -f ${OUTPUT[freemarker]} ]; then
+                        curl --output ${OUTPUT[freemarker]} ${URL[freemarker]}
+                fi
+                if [ ! -f ${UTILS}/freemarker.jar ]; then
+                        tar -C ${UTILS} -xzf ${DOWNLOADS}/freemarker.tgz freemarker-2.3.8/lib/freemarker.jar --strip=2
+                fi
                 "
 }
 
-get_source_jdk() {
-        init_source_jdk
-        fetch_branch_jdk
-}
-
-get_source_omr() {
-        init_source_omr
-        fetch_branch_omr
-}
-
-get_source_openj9() {
-        init_source_openj9
-        fetch_branch_openj9
-}
-
-extract_freemarker() {
-        [ ! -f "${UTILS}/freemarker.jar" ] &&
-                exec_or_die  "\
-                        tar -C ${UTILS} \
-                                -xzf ${DOWNLOADS}/freemarker.tgz freemarker-2.3.8/lib/freemarker.jar --strip=2 \
-                        "
-}
-
-extract_bootjdk() {
-        [ ! -d "${UTILS}/bootjdk${VERSION}_${ARCH}" ] &&
-                exec_or_die "\
-                        tar -xzf ${DOWNLOADS}/bootjdk${VERSION}_${ARCH}.tar.gz -C ${TMP_DIR} &&
+get_bootjdk() {
+        create_sh "
+                if [ ! -f ${OUTPUT[bootjdk]} ]; then
+                        curl --output ${OUTPUT[bootjdk]} ${URL[bootjdk]}
+                fi
+                if [ ! -d ${UTILS}/bootjdk${VERSION}_${ARCH} ]; then
+                        tar -xzf ${DOWNLOADS}/bootjdk${VERSION}_${ARCH}.tar.gz -C ${TMP_DIR}
                         mv ${TMP_DIR}/* ${UTILS}/bootjdk${VERSION}_${ARCH}
-                        "
+                fi
+                "
 }
 
 source_env() {
@@ -320,42 +277,22 @@ echo "Running With the following variables"
 print_script_env
 echo "========="
 
-pids=()
 
 # set env
-
-[ "_${XDOCKER}" == "_" ] && source_env
+source_env
 
 source "${SOURCE_FLAGS}"
 
-( get_freemarker && extract_freemarker )& pids+=( "$!" )
+get_freemarker
+get_bootjdk
+get_dockerfile
+get_watchdog
+get_xdocker
+get_source_jdk
+get_omr
+get_openj9
 
-( get_bootjdk && extract_bootjdk )& pids+=( "$!" )
-
-( get_dockerfile )& pids+=( "$!" )
-
-( get_watchdog )& pids+=( "$!" )
-
-( get_xdocker )& pids+=( "$!" )
-
-( get_source_jdk )& pids+=( "$!" )
-
-( get_source_omr )& pids+=( "$!" )
-
-( get_source_openj9 )& pids+=( "$!" )
-
-# Wait for all processes to finish
-EXIT_CODE=0
-for p in "${pids[@]}"; do
-        if wait "${p}"; then
-                echo "Process $p success"
-        else
-                EXIT_CODE=$(( 1 + EXIT_CODE ))
-                echo "Process $p fail"
-        fi
-done
-
-get_source 
+run_sh # spin it!
 
 patch_debug
 
